17cf877023a7ae52f0146f1b4670bb24c0145e32
Влад Зубанов <144595141+ktoyatakoy23@users.noreply.github.com>
2023-11-13 22:29:09 +0700

    Add files via upload

---
+++ тетрис winforms/тетрис/shape.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тетрис
+{
+    internal class shape
+    {
+        public int x;
+        public int y;
+        public int[,] matrix;
+
+        public shape(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+            matrix = new int[3, 3]
+            {
+                {0,1,0},
+                {0,1,1},
+                {0,0,1},
+            };
+        }
+
+        public void MoveDown()
+        {
+            y++;
+        }
+        public void MoveRight()
+        {
+            x++;
+        }
+        public void MoveLeft()
+        {
+            x--;
+        }
+    }
+}
