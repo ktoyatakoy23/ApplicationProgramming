e75294e0eccc3400cc160ed290d7b9e5b1600094
Влад Зубанов <144595141+ktoyatakoy23@users.noreply.github.com>
2023-11-13 22:29:29 +0700

    Add files via upload

---
+++ todo список WPF/задачник на wpf/Services2/FileIO.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using задачник_на_wpf.models;
+
+namespace задачник_на_wpf.Services2
+{
+    internal class FileIO
+    {
+        private readonly string PATH;
+        
+        public FileIO(string path)
+        {
+            PATH = path;
+        }
+
+        public BindingList<ToDoModel> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if(!fileExists) 
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<ToDoModel>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+            }
+        }
+       
+        public void SaveData(object _toDoData) 
+        {
+            using (StreamWriter writer = File.CreateText(PATH)) 
+            {
+                string output = JsonConvert.SerializeObject(_toDoData);
+                writer.Write(output);
+            }
+        }
+    }
+}
---
+++ todo список WPF/задачник на wpf/services/FileIOService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using задачник_на_wpf.models;
+
+namespace задачник_на_wpf.services
+{
+    class FileIOService
+    {
+        private readonly string path;
+
+        public FileIOService
+        
+        public BindingList<ToDoModel> LoadData()
+        {
+            return null;
+        }
+        public void SaveData(BindingList<ToDoModel> _toDoData) 
+        {
+
+        }
+    }
+}
