cf5484bf164e7a8cfdb976fc61baf9206dc033ae
Влад Зубанов <144595141+ktoyatakoy23@users.noreply.github.com>
2023-11-13 22:28:42 +0700

    Add files via upload

---
+++ графики WPF/графики/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace графики
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.textBox_h = new System.Windows.Forms.TextBox();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.delete = new System.Windows.Forms.Button();
+            this.build = new System.Windows.Forms.Button();
+            this.checkBox_sin = new System.Windows.Forms.CheckBox();
+            this.checkBox_cos = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.AccessibleDescription = "graph";
+            chartArea4.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            legend4.Title = "Графики";
+            this.chart.Legends.Add(legend4);
+            this.chart.Location = new System.Drawing.Point(6, 13);
+            this.chart.Name = "chart";
+            series7.BorderColor = System.Drawing.Color.White;
+            series7.BorderWidth = 2;
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            series7.Legend = "Legend1";
+            series7.LegendText = "cos";
+            series7.Name = "Series1";
+            series7.ShadowColor = System.Drawing.Color.White;
+            series7.YValuesPerPoint = 4;
+            series8.BorderWidth = 2;
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            series8.Legend = "Legend1";
+            series8.LegendText = "sin";
+            series8.Name = "Series2";
+            this.chart.Series.Add(series7);
+            this.chart.Series.Add(series8);
+            this.chart.Size = new System.Drawing.Size(575, 386);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            title4.Name = "Title1";
+            title4.Text = "Графики функций sin и cos";
+            this.chart.Titles.Add(title4);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(587, 407);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_b);
+            this.groupBox2.Controls.Add(this.textBox_h);
+            this.groupBox2.Controls.Add(this.textBox_a);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(605, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(222, 202);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " ";
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Location = new System.Drawing.Point(110, 63);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(100, 22);
+            this.textBox_b.TabIndex = 4;
+            // 
+            // textBox_h
+            // 
+            this.textBox_h.Location = new System.Drawing.Point(110, 92);
+            this.textBox_h.Name = "textBox_h";
+            this.textBox_h.Size = new System.Drawing.Size(100, 22);
+            this.textBox_h.TabIndex = 3;
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(110, 32);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(100, 22);
+            this.textBox_a.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(21, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " H (шаг) =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(21, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " В (до)  =";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(21, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " А (от)  =";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.delete);
+            this.groupBox3.Controls.Add(this.build);
+            this.groupBox3.Controls.Add(this.checkBox_sin);
+            this.groupBox3.Controls.Add(this.checkBox_cos);
+            this.groupBox3.Location = new System.Drawing.Point(605, 220);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(222, 199);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Выбор графика";
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.delete.Location = new System.Drawing.Point(25, 142);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(185, 44);
+            this.delete.TabIndex = 3;
+            this.delete.Text = "Стереть";
+            this.delete.UseVisualStyleBackColor = false;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // build
+            // 
+            this.build.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.build.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.build.Location = new System.Drawing.Point(25, 92);
+            this.build.Name = "build";
+            this.build.Size = new System.Drawing.Size(185, 44);
+            this.build.TabIndex = 2;
+            this.build.Text = "Построить график";
+            this.build.UseVisualStyleBackColor = false;
+            this.build.Click += new System.EventHandler(this.build_Click);
+            // 
+            // checkBox_sin
+            // 
+            this.checkBox_sin.AutoSize = true;
+            this.checkBox_sin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox_sin.Location = new System.Drawing.Point(25, 61);
+            this.checkBox_sin.Name = "checkBox_sin";
+            this.checkBox_sin.Size = new System.Drawing.Size(101, 24);
+            this.checkBox_sin.TabIndex = 1;
+            this.checkBox_sin.Text = "y = sin(x)";
+            this.checkBox_sin.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_cos
+            // 
+            this.checkBox_cos.AutoSize = true;
+            this.checkBox_cos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox_cos.Location = new System.Drawing.Point(25, 31);
+            this.checkBox_cos.Name = "checkBox_cos";
+            this.checkBox_cos.Size = new System.Drawing.Size(106, 24);
+            this.checkBox_cos.TabIndex = 0;
+            this.checkBox_cos.Text = "y = cos(x)";
+            this.checkBox_cos.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(836, 434);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Графики";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.TextBox textBox_h;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox_sin;
+        private System.Windows.Forms.CheckBox checkBox_cos;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button build;
+    }
+}
+
---
+++ графики WPF/графики/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace графики
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double a, b, h;
+        private double x, y;
+
+        
+
+        private void build_Click(object sender, EventArgs e)
+        {
+            if (checkBox_cos.Checked == false && checkBox_sin.Checked == false)
+            {
+                MessageBox.Show("Выбери хотя бы один график", "Внимание!");
+                return;
+            }
+
+            if (textBox_a.Text == "" && textBox_b.Text == "" || textBox_h.Text == "")
+            {
+                MessageBox.Show("Параметры по умолчанию", "Внимание!");
+                Defaults();
+            }
+            else
+            {
+                a = Convert.ToDouble(textBox_a.Text);
+                b = Convert.ToDouble(textBox_b.Text);
+                h = Convert.ToDouble(textBox_h.Text);
+
+            }
+            if (checkBox_cos.Checked)
+            {
+                x = a;
+                this.chart.Series[0].Points.Clear();
+
+                while (x <= b)
+                {
+                    y = Math.Cos(x);
+                    this.chart.Series[0].Points.AddXY(x, y);
+                    x += h;
+                }
+            }
+            if (checkBox_sin.Checked)
+            {
+                x = a;
+                this.chart.Series[1].Points.Clear();
+
+                while (x <= b)
+                {
+                    y = Math.Sin(x);
+                    this.chart.Series[1].Points.AddXY(x, y);
+                    x += h;
+                }
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            if (checkBox_cos.Checked == false && checkBox_sin.Checked == false)
+            {
+                MessageBox.Show("Выбери хотя бы один график", "Внимание!");
+                return;
+            }
+            if (checkBox_sin.Checked)
+            {
+                this.chart.Series[1].Points.Clear();
+            }
+            if (checkBox_cos.Checked)
+            {
+                this.chart.Series[0].Points.Clear();
+            }
+        }
+     
+        private void Defaults()
+        {
+            a = -10;
+            b = 10;
+            h = 0.1;
+        }
+    }
+}
